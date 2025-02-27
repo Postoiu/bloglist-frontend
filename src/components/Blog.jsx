afbc9fbe1d657a35e57869ae60ac8801e00fce22
Ovidiu Postoiu <postoiu.o@gmail.com>
2025-02-27 10:45:29 +0200

    Exercises 5.1 - 5.12

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,50 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({
+  blog,
+  currentUser,
+  onUpdate,
+  onRemove
+}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [showDetails, setShowDetails] = useState(false)
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setShowDetails(!showDetails)}>{showDetails ? 'hide' : 'view'}</button>
+      </div>
+      {
+        showDetails &&
+        <div>
+          <div>
+            {blog.url}
+          </div>
+          <div>
+            {`likes ${blog.likes}`}
+            <button onClick={() => onUpdate(blog.id)}>like</button>
+          </div>
+          <div>
+            {blog.user.name}
+          </div>
+          <div>
+            {
+              currentUser.username === blog.user.username &&
+              <button onClick={() => onRemove(blog.id)}>remove</button>
+            }
+          </div>
+        </div>
+      }
+    </div>
+  )
+}
 
 export default Blog
